6510dd4112bb8344b3df8b0b9478e4729ab6bdae
elhidraouij <jawad.elhidraoui.pro@gmail.com>
2025-08-19 16:12:05 +0200

    continue iterators tp

--- tp07-iterators/main.cpp
+++ tp07-iterators/main.cpp
@@ -14,9 +14,25 @@ int main() {
 
     // To access the next element we use ++iterator
     // To access the previous element we use --iterator
+    std::cout << "Iterate forward: ";
     for (std::vector<int>::iterator it { std::begin(int_vector) }; it != std::end(int_vector); ++it) {
-        std::cout << *it << std::endl;
+        std::cout << *it;
     }
+    std::cout << std::endl;
+
+    // To iterate from the end to the beginning
+    std::cout << "Iterate backward: ";
+    for (std::vector<int>::reverse_iterator it { std::rbegin(int_vector) };  it != std::rend(int_vector); ++it) {
+        std::cout << *it;
+    }
+    std::cout << std::endl;
+
+    // Create subset of other collections
+    std::vector<int> my_subset { std::begin(int_vector) + 1, std::begin(int_vector) + 2 };
+    for (int n : my_subset) {
+        std::cout << n;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
